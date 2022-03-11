75f94d25194966230d0857b2b8b1fa73d0d34534
Daniel <danoladesign@gmail.com>
2022-03-11 10:01:06 -0600

    frist commit

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
+import {YELP_API_KEY} from '@env'
 
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
     Authorization:
-      'Bearer 5M15dpk2DjYi9zp2UpeQx7k6AwejguKPfeI2IYuHnw__mxdZnO_6zLIeGpIqay2mMpRi8H7EzW-ceI_9d6MlukAdR1h-303tnIy3K6aHczMHajHcplwRxB65KJwpYnYx'
+      `${YELP_API_KEY}`
   }
 })
